74713a5154b1d85b7967e2c020fda4e88fbdffa4
Devesh <Devesh.yadav@floweraura.com>
2025-03-29 14:29:58 +0530

    initial commit

---
+++ src/bottom-section.tsx
@@ -0,0 +1,11 @@
+import { Box, Center } from '@chakra-ui/react';
+
+export function BottomSection() {
+  return (
+    <Box pb='2'>
+      <Center fontSize='xs' color='fg.muted'>
+        LegalAssist can make mistakes. Check important info.
+      </Center>
+    </Box>
+  );
+}
